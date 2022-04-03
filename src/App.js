5c1feaefd237177e966a49ad87b080622c1a61f1
shuklahimanshu81 <70198636+shuklahimanshu81@users.noreply.github.com>
2022-04-03 17:07:22 +0530

    feat : Added routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Footer from "./Components/Home/Footer";
-import Header from "./Components/Home/Header";
+import Header from "./Components/Home/Header"
 import Landing from "./Components/Home/Landing";
+import Footer from "./Components/Home/Footer";
+import Products from "./Components/Products/Products";
+
 
 function App() {
   
   return (
     <div className="App">
       <Header />
-      <Landing />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path='/products' element={<Products />} />
+      </Routes>
       <Footer />
     </div>
   );
