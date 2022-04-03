84f15218f533034986cc8d84a40d80a3780c759c
shuklahimanshu81 <70198636+shuklahimanshu81@users.noreply.github.com>
2022-04-03 22:34:51 +0530

    feat : rendered products on the products page.

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,43 +1,48 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-const Products = () => {
-const [productList, setProductList] = useState([])
+//CSS
+import './Products.css'
 
-useEffect(() => {
-(async () => {
-try {
-const res = await axios.get("/api/products");
-setProductList(res.data.products);
-} catch (error) {
-console.log(error);
-}
-})();
-}, [])
+const Products = () => {
+    const [productList, setProductList] = useState([])
+
+    useEffect(() => {
+        (async () => {
+        try {
+              const res = await axios.get("/api/products");
+              setProductList(res.data.products);
+        } catch (error) {
+              console.log(error);
+        }
+      })();
+    }, [])
+
+
+
 return <div className="products-page">
-  {
-  productList.map( product =>{
-  return <div>
-    <div className="vertical-card card-section">
-      <div className="card-img-section">
-        <img src={product.image} alt="alt-text" className="card-img" />
-      </div>
-      <div className="card-heading">
-        {product.title}
-      </div>
-      <div className="card-subheading">
-      </div>
-      <div className="card-detail">{product.price} </div>
-      <div className="card-btns">
-        <button className="card-primary-btn card-btn"> Add to cart</button>
-        <button className="card-secondary-btn card-btn"> Buy Now </button>
-      </div>
-    </div>
-  </div>
-  }
-  )
-  }
+<section className="products-section">
+    {
+      productList.map( product =>{
+        return <div className="vertical-card card-section">
+          <div className="card-img-section">
+            <img src={product.image} alt={product.title} className="card-img" />
+          </div>
+          <div className="card-heading">
+            {product.title}
+          </div>
+          <div className="card-detail">{product.price} </div>
+          <div className="card-btns">
+            <button className="card-primary-btn card-btn"> Add to cart</button>
+            <button className="card-secondary-btn card-btn"> Add to wishlist </button>
+          </div>
+        </div>
+      }
+      )
+    }
+</section>
 </div>
+
 }
 
 export default Products;
