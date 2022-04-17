36fa0948414e3c6c49fefbf2e85e65d0a2052904
shuklahimanshu81 <70198636+shuklahimanshu81@users.noreply.github.com>
2022-04-18 01:29:03 +0530

    feat : Added all the providers

--- src/index.js
+++ src/index.js
@@ -4,15 +4,25 @@ import "./index.css";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
+import {CartProvider} from "./Context/cartContext";
+import { FilterProvider } from "./Context/filterContext";
+import { WishlistProvider } from "./Context/wishlistContext";
 
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
+    <FilterProvider>
+    <CartProvider>
+      <WishlistProvider>
     <BrowserRouter>
          <App />
-    </BrowserRouter>   
+    </BrowserRouter> 
+    </WishlistProvider>
+    </CartProvider>
+    </FilterProvider>
+      
   </React.StrictMode>,
   document.getElementById("root")
 );
