46e7115e3f345fc646fa0f0fc4eec8344d7371dd
shuklahimanshu81 <70198636+shuklahimanshu81@users.noreply.github.com>
2022-04-18 01:26:02 +0530

    feat : products page consisting all products

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,31 +1,24 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
+
+import { useCart } from "../../Context/cartContext";
+import {  useProduct } from "../../Context/filterContext";
+import { useWishlist } from "../../Context/wishlistContext";
 import Filters from "../Filters/Filters";
 
 //CSS
 import './Products.css'
 
 const Products = () => {
-    const [productList, setProductList] = useState([])
-
-    useEffect(() => {
-        (async () => {
-        try {
-              const res = await axios.get("/api/products");
-              setProductList(res.data.products);
-        } catch (error) {
-              console.log(error);
-        }
-      })();
-    }, [])
-
-
+    
+ const { finalProduct} = useProduct()
+ const {cartDispatch} = useCart();
+const {wishlistDispatch} = useWishlist()
+console.log(finalProduct)
 
 return <div className="products-page">
   <Filters />
 <section className="products-section">
     {
-      productList.map( product =>{
+     finalProduct.map( product =>{
         return <div className="vertical-card card-section">
           <div className="card-img-section">
             <img src={product.image} alt={product.title} className="card-img" />
@@ -35,8 +28,8 @@ return <div className="products-page">
           </div>
           <div className="card-detail">{product.price} </div>
           <div className="card-btns">
-            <button className="card-primary-btn card-btn"> Add to cart</button>
-            <button className="card-secondary-btn card-btn"> Add to wishlist </button>
+            <button className="card-primary-btn card-btn" onClick={()=> cartDispatch({type:"ADD_TO_CART",payload: product})}> Add to cart</button>
+            <button className="card-secondary-btn card-btn" onClick={()=> wishlistDispatch({type:"ADD_TO_WISHLIST",payload: product})}> Add to wishlist </button>
           </div>
         </div>
       }
