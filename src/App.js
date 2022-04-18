b5836fb1405470aa4bb1edc9d8ee5b5650c5abec
shuklahimanshu81 <70198636+shuklahimanshu81@users.noreply.github.com>
2022-04-18 23:57:32 +0530

    fix : merge conflict resolve

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import "./App.css";
 import Footer from "./Components/Home/Footer";
 import Header from "./Components/Home/Header";
 import Landing from "./Components/Home/Landing";
+import Products from "./Components/Products/Products";
+import Cart from "./Components/Cart/cart";
+import Login from "./Components/Login/Login";
+import Signup from "./Components/Signup/Signup";
+import Wishlist from "./Components/wishlist/wishlist";
+
 
 function App() {
   
