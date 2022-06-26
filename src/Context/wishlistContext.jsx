77bc67a892a0948d70090a40a4bdf87a482765e4
shuklahimanshu81 <70198636+shuklahimanshu81@users.noreply.github.com>
2022-06-26 11:54:15 +0530

    feat : context for filter,cart and wishlist

--- src/Context/wishlistContext.jsx
+++ src/Context/wishlistContext.jsx
@@ -1,5 +1,5 @@
 
-import {createContext, useContext, useReducer} from 'react'
+import { createContext, useContext, useReducer } from 'react'
 import { useProduct } from './filterContext';
 
 const wishlistContext = createContext();
@@ -8,22 +8,22 @@ const useWishlist = () => useContext(wishlistContext)
 
 
 
-const WishlistProvider = ({children}) => {
-  const {productList} = useProduct();
+const WishlistProvider = ({ children }) => {
+    const { productList } = useProduct();
 
-  const addToWishlist = (wishlistState,action) => { 
-     let data = [...wishlistState.wishlist,{...action.payload}] 
-     wishlistState= {...wishlistState, wishlist:data}
-     return wishlistState
+    const addToWishlist = (wishlistState, action) => {
+        let data = [...wishlistState.wishlist, { ...action.payload }]
+        wishlistState = { ...wishlistState, wishlist: data }
+        return wishlistState
     }
-    
-  
-    const [wishlistState,wishlistDispatch]= useReducer(addToWishlist,{wishlist:[]})
-    console.log(wishlistState)  
 
-    return <wishlistContext.Provider value={{wishlistState,wishlistDispatch}}>
+
+    const [wishlistState, wishlistDispatch] = useReducer(addToWishlist, { wishlist: [] })
+    // console.log(wishlistState)  
+
+    return <wishlistContext.Provider value={{ wishlistState, wishlistDispatch }}>
         {children}
     </wishlistContext.Provider>
 }
 
-export {WishlistProvider , useWishlist};
+export { WishlistProvider, useWishlist };
