6656a883f7f1b7ceb2ff4895aef2b93c5b2b5472
shuklahimanshu81 <70198636+shuklahimanshu81@users.noreply.github.com>
2022-07-01 00:09:53 +0530

    feat : Cart management & Css related to cart

--- src/Components/Cart/cart.jsx
+++ src/Components/Cart/cart.jsx
@@ -1,35 +1,118 @@
-import {useCart} from '../../Context//cartContext'
-import './cart.css'
+import { useCart } from "../../Context//cartContext";
+import { useWishlist } from "../../Context/wishlistContext";
+import "./cart.css";
+import { useNavigate } from "react-router-dom";
 
 const Cart = () => {
-
-const {cartState} = useCart();
-
-
-    return <main className="main-div-cart">
-      
-      <section className="products-section">
-    {
-     cartState.cart.map( product =>{
-        return <div className="vertical-card card-section">
-          <div className="card-img-section">
-            <img src={product.image} alt={product.title} className="card-img" />
-          </div>
-          <div className="card-heading">
-            {product.title}
-          </div>
-          <div className="card-detail">{product.price} </div>
-          <div className="card-btns">
-            <button className="card-primary-btn card-btn" > Remove from cart</button>
-            <button className="card-secondary-btn card-btn" > Move to wishlist </button>
+  const { cartState, cartDispatch } = useCart();
+  const { wishlistDispatch } = useWishlist();
+  const navigate = useNavigate();
+  let totalPrice = 0;
+  cartState.cart.forEach((item) => {
+    totalPrice = totalPrice + item.price * item.quantity;
+  });
+  return (
+    <main className="cart">
+      {cartState.cart.length > 0 ? (
+        <div className="main-div-cart">
+          <section className="products-section">
+            {cartState.cart.map((product, index) => {
+              return (
+                <div key={index} className="horizontal-card">
+                  <div className="card-img-section">
+                    <img
+                      src={product.image}
+                      alt={product.title}
+                      className="hz-card-img cart-img"
+                    />
+                  </div>
+                  <div className="about-card">
+                    <p className="card-heading">{product.title}</p>
+                    <p className="card-detail"> ₹ {product.price} </p>
+                    <p className="card-btn-section">
+                      <button
+                        className="card-btn cart-btn"
+                        onClick={() =>
+                          cartDispatch({
+                            type: "DECREASE_QUANTITY",
+                            payload: product,
+                          })
+                        }
+                      >
+                        -
+                      </button>
+                      {product.quantity}
+                      <button
+                        className="card-btn cart-btn"
+                        onClick={() =>
+                          cartDispatch({
+                            type: "INCREASE_QUANTITY",
+                            payload: product,
+                          })
+                        }
+                      >
+                        +
+                      </button>
+                    </p>
+                    <p className="card-btns">
+                      <button
+                        className="card-primary-btn card-btn"
+                        onClick={() => {
+                          wishlistDispatch({
+                            type: "ADD_TO_WISHLIST",
+                            payload: product,
+                          });
+                          cartDispatch({
+                            type: "REMOVE_FROM_CART",
+                            payload: product,
+                          });
+                        }}
+                      >
+                        Move to wishlist
+                      </button>
+                      <button
+                        className="card-secondary-btn card-btn"
+                        onClick={() =>
+                          cartDispatch({
+                            type: "REMOVE_FROM_CART",
+                            payload: product,
+                          })
+                        }
+                      >
+                        Remove
+                      </button>
+                    </p>
+                    <p></p>
+                  </div>
+                </div>
+              );
+            })}
+          </section>
+          <div class="text-only-card">
+            <p class="card-heading">Price Details</p>
+            <hr></hr>
+            <section className="total-price-section">
+              <p className="totol-price">Total Price : {totalPrice}</p>
+              <button className="card-primary-btn card-btn place-order-btn">
+                Place Order
+              </button>
+            </section>
           </div>
         </div>
-      }
-      )
-    }
-</section>
-      
+      ) : (
+        <div className="empty-cart-div">
+          <p className="empty-cart-message">
+            {" "}
+            Looks like you didn't added anything to your cart.
+          </p>
+          <p className="shop-btn" onClick={() => navigate("/products")}>
+            {" "}
+            Shop Now{" "}
+          </p>
+        </div>
+      )}
     </main>
-}
+  );
+};
 
-export default Cart ;
+export default Cart;
