418d413c802faff9bb87d8f0f98dd97e4ed96c53
shuklahimanshu81 <70198636+shuklahimanshu81@users.noreply.github.com>
2022-07-01 00:08:55 +0530

    fix : converted price to number from string

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -1,139 +1,154 @@
 import { v4 as uuid } from "uuid";
 export const products = [
-    {
-        _id: uuid(),
+  {
+    _id: uuid(),
 
-        title:"Drum",
-        rating:'3',
-        image :'https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Drum.jpg',  
-        price: "5000",
-        categoryName: "drum",
-      },
-      {
-        _id: uuid(),
+    title: "Drum",
+    rating: "3",
+    image:
+      "https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Drum.jpg",
+    price: 5000,
+    categoryName: "drum",
+  },
+  {
+    _id: uuid(),
 
-        title:"Flute",
-        rating:'3',
-        image :'https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Flute.jpg',  
-        price: "3000",
-        categoryName: "flute",
-      },
-      {
-        _id: uuid(),
+    title: "Flute",
+    rating: "3",
+    image:
+      "https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Flute.jpg",
+    price: 3000,
+    categoryName: "flute",
+  },
+  {
+    _id: uuid(),
 
-        title:"Drum",
-        rating:'5',
-        image :'https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Drum.jpg',  
-        price: "6000",
-        categoryName: "drum",
-      },
-      {
-        _id: uuid(),
+    title: "Drum",
+    rating: "5",
+    image:
+      "https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Drum.jpg",
+    price: 6000,
+    categoryName: "drum",
+  },
+  {
+    _id: uuid(),
 
-        title:"Drum",
-        rating:'3',
-        image :'https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Drum.jpg',  
-        price: "1200",
-        categoryName: "drum",
-      },
-      {
-        _id: uuid(),
+    title: "Drum",
+    rating: "3",
+    image:
+      "https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Drum.jpg",
+    price: 1200,
+    categoryName: "drum",
+  },
+  {
+    _id: uuid(),
 
-        title:"Trumpet",
-        rating:'4',
-        image :'https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Trumpet.jpg',  
-        price: "5000",
-        categoryName: "trumpet",
-      },
-      {
-        _id: uuid(),
+    title: "Trumpet",
+    rating: "4",
+    image:
+      "https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Trumpet.jpg",
+    price: 5000,
+    categoryName: "trumpet",
+  },
+  {
+    _id: uuid(),
 
-        title:"Trumpet",
-        rating:'2',
-        image :'https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Trumpet.jpg',  
-        price: "5300",
-        categoryName: "trumpet",
-      },
-      
-      {
-        _id: uuid(),
+    title: "Trumpet",
+    rating: "2",
+    image:
+      "https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Trumpet.jpg",
+    price: 5300,
+    categoryName: "trumpet",
+  },
 
-        title:"Flute",
-        rating:'3',
-        image :'https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Flute.jpg',  
-        price: "5000",
-        categoryName: "flute",
-      },
-      {
-        _id: uuid(),
+  {
+    _id: uuid(),
 
-        title:"Flute",
-        rating:'4',
-        image :'https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Flute.jpg',  
-        price: "5000",
-        categoryName: "flute",
-      },
-      {
-        _id: uuid(),
+    title: "Flute",
+    rating: "3",
+    image:
+      "https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Flute.jpg",
+    price: 5000,
+    categoryName: "flute",
+  },
+  {
+    _id: uuid(),
 
-        title:"Drum",
-        rating:'3',
-        image :'https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Drum.jpg',  
-        price: "5000",
-        categoryName: "drum",
-      },
-      {
-        _id: uuid(),
+    title: "Flute",
+    rating: "4",
+    image:
+      "https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Flute.jpg",
+    price: 5000,
+    categoryName: "flute",
+  },
+  {
+    _id: uuid(),
 
-        title:"Flute",
-        rating:'1',
-        image :'https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Flute.jpg',  
-        price: "5000",
-        categoryName: "flute",
-      },
-      {
-        _id: uuid(),
+    title: "Drum",
+    rating: "3",
+    image:
+      "https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Drum.jpg",
+    price: 5000,
+    categoryName: "drum",
+  },
+  {
+    _id: uuid(),
 
-        title:"Trumpet",
-        rating:'3',
-        image :'https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Trumpet.jpg',  
-        price: "5000",
-        categoryName: "trumpet",
-      },
-      {
-        _id: uuid(),
+    title: "Flute",
+    rating: "1",
+    image:
+      "https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Flute.jpg",
+    price: 5000,
+    categoryName: "flute",
+  },
+  {
+    _id: uuid(),
 
-        title:"Drum",
-        rating:'5',
-        image :'https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Drum.jpg',  
-        price: "5199",
-        categoryName: "drum",
-      },
-      {
-        _id: uuid(),
+    title: "Trumpet",
+    rating: "3",
+    image:
+      "https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Trumpet.jpg",
+    price: 5000,
+    categoryName: "trumpet",
+  },
+  {
+    _id: uuid(),
 
-        title:"Trumpet",
-        rating:'3',
-        image :'https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Trumpet.jpg',  
-        price: "5000",
-        categoryName: "trumpet",
-      },
-      {
-        _id: uuid(),
+    title: "Drum",
+    rating: "5",
+    image:
+      "https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Drum.jpg",
+    price: 5199,
+    categoryName: "drum",
+  },
+  {
+    _id: uuid(),
 
-        title:"Flute",
-        rating:'3',
-        image :'https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Flute.jpg',  
-        price: "5000",
-        categoryName: "flute",
-      },
-      {
-        _id: uuid(),
+    title: "Trumpet",
+    rating: "3",
+    image:
+      "https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Trumpet.jpg",
+    price: 5000,
+    categoryName: "trumpet",
+  },
+  {
+    _id: uuid(),
 
-        title:"Trumpet",
-        rating:'5',
-        image :'https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Trumpet.jpg',  
-        price: "5000",
-        categoryName: "trumpet",
-      },
+    title: "Flute",
+    rating: "3",
+    image:
+      "https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Flute.jpg",
+    price: 5000,
+    categoryName: "flute",
+  },
+  {
+    _id: uuid(),
+
+    title: "Trumpet",
+    rating: "5",
+    image:
+      "https://raw.githubusercontent.com/shuklahimanshu81/Blossom-store-react-app/Dev/blossom-store/src/Assets/Trumpet.jpg",
+    price: 5000,
+    categoryName: "trumpet",
+  },
 ];
