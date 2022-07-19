6a7d134beaae7cbe691e38299f724d025b340a8e
shuklahimanshu81 <70198636+shuklahimanshu81@users.noreply.github.com>
2022-07-19 14:50:15 +0530

    chore : updated demo user password according to format

--- src/backend/db/users.js
+++ src/backend/db/users.js
@@ -12,7 +12,7 @@ export const users = [
     firstName: "Adarsh",
     lastName: "Balika",
     email: "adarshbalika@gmail.com",
-    password: "adarshbalika",
+    password: "Adarshbalika@123",
     createdAt: formatDate(),
     updatedAt: formatDate(),
   },
