c4a89450f0c25736ba114253dff14ca2a1a6a351
shuklahimanshu81 <70198636+shuklahimanshu81@users.noreply.github.com>
2022-07-19 14:51:57 +0530

    feat : Added authcontext to Navbar

--- src/Components/Home/Header.jsx
+++ src/Components/Home/Header.jsx
@@ -1,49 +1,59 @@
-import logo from '../../Assets/Images/store-logo.png'
-import { FaRegHeart,FaShoppingCart } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-import { useCart } from '../../Context/cartContext';
-import { useWishlist } from '../../Context/wishlistContext';
-
+import logo from "../../Assets/Images/store-logo.png";
+import { FaRegHeart, FaShoppingCart } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { useCart } from "../../Context/cartContext";
+import { useWishlist } from "../../Context/wishlistContext";
+import { useAuth } from "../../Context/authContext";
 
 const Header = () => {
-
-  const {cartState} = useCart();
-  const {wishlistState} = useWishlist();
-    return( 
-<nav className="nav-component">
+  const { cartState } = useCart();
+  const { wishlistState } = useWishlist();
+  const { authToken, logoutUser } = useAuth();
+  return (
+    <nav className="nav-component">
       <div>
-          <Link to={"/"}><img src={logo}
-            alt="Blossom" className="logo-img" />
-          </Link>
+        <Link to={"/"}>
+          <img src={logo} alt="Blossom" className="logo-img" />
+        </Link>
       </div>
       <div className="nav-center">
-        <input className="search-bar" type="text" placeholder="Search Here..." />
+        <input
+          className="search-bar"
+          type="text"
+          placeholder="Search Here..."
+        />
       </div>
-    <div className="right-nav">
-     <ul className="nav-items">
-       <li className="list-item">
-        <Link to={"/login"}><button className="primary-btn btn">Log In </button></Link>
-       </li>
-       <li className="list-item">
-       <Link to={"/signup"}><button className="secondary-btn btn">Sign Up</button></Link>
-       </li>
-       <li className="list-item">
-         <Link className="item flex" to={"/cart"}>
-           <FaShoppingCart />
-           <p className='cart-count'>{cartState.cart.length}</p>
-         </Link>
-       </li>
-       <li>
-          <Link className="item flex" to={"/wishlist"} >
-            <FaRegHeart height="3em" />
-            <p className='wishlist-count'> {wishlistState.wishlist.length} </p>
-          </Link>
-       </li>
-     </ul>
-   </div>
-</nav>
-    )
-    
-}
+      <div className="right-nav">
+        <ul className="nav-items">
+          {authToken ? (
+            <li className="list-item">
+              <button className="secondary-btn btn" onClick={logoutUser}>
+                Logout
+              </button>
+            </li>
+          ) : (
+            <li className="list-item">
+              <Link to={"/login"}>
+                <button className="primary-btn btn">Log In / Sign Up </button>
+              </Link>
+            </li>
+          )}
+          <li className="list-item">
+            <Link className="item flex" to={"/cart"}>
+              <FaShoppingCart />
+              <p className="cart-count">{cartState.cart.length}</p>
+            </Link>
+          </li>
+          <li>
+            <Link className="item flex" to={"/wishlist"}>
+              <FaRegHeart height="3em" />
+              <p className="wishlist-count">{wishlistState.wishlist.length}</p>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Header
+export default Header;
